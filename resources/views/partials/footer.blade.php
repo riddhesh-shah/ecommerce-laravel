0d5ee6c4b11aba2d6cd63792ce09bbc1f603a6b3
Riddhesh Shah <riddheshshah1203@gmail.com>
2022-12-09 18:22:24 +0530

    final commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,26 @@
+<footer class="bg-dark">
+    <div class="container bg-dark">
+        <div class="footer">
+            <div class="row">
+                <div class="col-md-4">
+                    <a href="#" class="footer-link">Privacy policy</a><br>
+                    <a href="#" class="footer-link">Term and conditions</a><br>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Welcome to the site
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquam, assumenda. Culpa, libero.</p>
+                </div>
+                <div class="col-md-4">
+                    <h3>
+                        Contact:
+                    </h3>
+                    <p>Phone: +919999999999</p>
+                    <p>Email: abcd1234@gmail.com</p>
+                </div>
+            </div>
+        </div>
+        <p>copyright &copy; All rights reserved 2022</p>
+    </div>
+</footer>
